498d40afa3f2ee1dca553a0a6467ed74ebb4eefb
dreamxinxcode <brandon@dreamincode.dev>
2023-11-11 03:47:05 -0800

    KM-2 - Dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SettingsService } from './services/settings/settings.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(private settingsService: SettingsService) {}
+
+  ngOnInit(): void {
+    // Check if the app has been installed before
+    const isAppInstalled = localStorage.getItem('isAppInstalled');
+
+    if (!isAppInstalled) {
+      // Set the dark theme for the first time
+      this.settingsService.toggleDarkMode();
+
+      // Mark the app as installed to avoid setting the theme on every app open
+      localStorage.setItem('isAppInstalled', 'true');
+    }
+  }
 }
