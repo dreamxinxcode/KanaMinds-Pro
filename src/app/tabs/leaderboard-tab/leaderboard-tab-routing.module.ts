0cdf5820ae0f2a0439514042da2c9b6a754314ab
dreamxinxcode <brandon@dreamincode.dev>
2023-11-16 18:36:32 -0800

    KM-10 - Leaderboard tab

--- src/app/tabs/leaderboard-tab/leaderboard-tab-routing.module.ts
+++ src/app/tabs/leaderboard-tab/leaderboard-tab-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LearnTabPage } from './learn-tab.page';
+import { LeaderboardTabPage } from './leaderboard-tab.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: LearnTabPage,
+    component: LeaderboardTabPage,
   }
 ];
 
@@ -13,4 +13,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class LearnTabPageRoutingModule {}
+export class LeaderboardTabPageRoutingModule {}
