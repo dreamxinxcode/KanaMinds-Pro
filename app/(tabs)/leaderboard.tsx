89656cbe6718979c2acec65adef7839f5e7e3d8c
dreamxinxcode <dreamxinxcode@gmail.com>
2025-02-10 21:54:10 -0800

    Remove leaderboard item shadow

--- app/(tabs)/leaderboard.tsx
+++ app/(tabs)/leaderboard.tsx
@@ -58,10 +58,6 @@ const styles = StyleSheet.create({
     padding: 10,
     width: '100%',
     marginBottom: 5,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 1 },
-    shadowOpacity: 0.22,
-    shadowRadius: 2.22,
     elevation: 3,
   }
 });
